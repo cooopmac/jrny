879901331606b73e6f25c3cae0656414f917bf2b
Cooper <cooper.macgregor14@gmail.com>
2025-05-29 17:34:15 -0400

    refresh

--- app/auth-loading.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function AuthLoadingScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Auth Loading Screen</Text>
-    </View>
-  );
-}
